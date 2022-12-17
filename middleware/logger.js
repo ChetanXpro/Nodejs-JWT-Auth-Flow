f569a4bc54a29df9d94aa6be9cd72504713baf46
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-12-17 21:25:40 +0530

    Update logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -9,22 +9,22 @@ const logEvents = async (message, logFileName) => {
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
   try {
-    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
-      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
-    }
+    // if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+    //   await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
+    // }
 
-    await fsPromises.appendFile(
-      path.join(__dirname, "..", "logs", logFileName),
-      logItem
-    );
+    // await fsPromises.appendFile(
+    //   path.join(__dirname, "..", "logs", logFileName),
+    //   logItem
+    // );
   } catch (error) {
     console.log(error);
   }
 };
 
 const logger = (req, res, next) => {
-  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
-  console.log(`${req.method} ${req.path}`);
+  // logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  // console.log(`${req.method} ${req.path}`);
 
   next();
 };
