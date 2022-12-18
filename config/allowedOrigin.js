ee8df4eb49a23ce6801394f93f79b153ab04ef96
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-12-18 23:42:22 +0530

    Update allowedOrigin.js

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,2 +1,2 @@
-const allowedOrigins = ["http://localhost:3000","https://frontend-role-auth-flow.vercel.app"];
+const allowedOrigins = ["http://localhost:3000","https://frontend-role-auth-flow.vercel.app","http://localhost:5173"];
 module.exports = allowedOrigins;
